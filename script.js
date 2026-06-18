cf90edd57a111005d23ece76db8041bb6f123381
marqueskennedy-netizen <marques.kennedy@escola.pr.gov.br>
2026-06-18 12:30:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 var titulo2 = document.querySelector("h2")
 titulo2.textContent = "Clientes";
+
+let paciente = document.querySelector("#primeiro-paciente");
+let tdPeso = paciente.querySelector(".info-peso");
+
+console.log(paciente);
+console.log(tdPeso);
+
