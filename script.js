bd1ac9ef14fa52519048b938bb53778f6f54be73
marqueskennedy-netizen <marques.kennedy@escola.pr.gov.br>
2026-06-19 12:32:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,27 @@ titulo2.textContent = "Clientes";
 
 let paciente = document.querySelector("#primeiro-paciente");
 let tdPeso = paciente.querySelector(".info-peso");
+let valorPeso = tdPeso.textContent;
 
-console.log(paciente);
-console.log(tdPeso);
+/*acecando a tag td da altura e buscando a altura* */
+let tdAltura = paciente.querySelector(".info-peso");
+let valorAltura = tdAltura.textContent;
 
+/*Equação para calculo do IMC* */
+let imc = valorPeso / (valorAltura * valorAltura)
+
+/**adição do valor Imc na tabela */
+let tdImc = paciente.querySelector('.info-imc');
+tdImc.textContent = imc
+
+/**Estrutura de verificação de peso e altura */
+if(valorPeso < 0 || valorPeso > 350){
+    alert("Peso Inválido!");
+
+
+}
+if(valorAltura < 0 || valorAltura > 3.000){
+    alert("Altura Inválida!");
+
+
+}
